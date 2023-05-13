94c8c68cdb05c6852c9eb0d072a8db1a4a226622
Ribo <ribo.ha1111@gmail.com>
2023-05-13 15:17:04 +0900

    feat: DTO에 제약 조건 추가

--- src/main/java/com/bulletinboard/post/dto/PostUpdateRequest.java
+++ src/main/java/com/bulletinboard/post/dto/PostUpdateRequest.java
@@ -4,10 +4,18 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 @Getter @Setter
 public class PostUpdateRequest {
 
+    @NotBlank(message = "제목은 필수입니다.")
+    @Size(min = 1, max = 15, message = "제목은 1글자 이상 15글자 이햐여야 합니다.")
     private String title;
+
+    @NotBlank(message = "내용은 필수입니다.")
+    @Size(min = 1, max = 1000, message = "제목은 1글자 이상 1000글자 이하여야 합니다.")
     private String content;
 
     protected PostUpdateRequest() {
