9543018346706119dc01eafe55ecfebe0e0cdffd
Ribo <ribo.ha1111@gmail.com>
2023-05-13 16:55:47 +0900

    style: 오류 수정

--- src/main/java/com/bulletinboard/post/dto/PostUpdateRequest.java
+++ src/main/java/com/bulletinboard/post/dto/PostUpdateRequest.java
@@ -11,11 +11,11 @@ import javax.validation.constraints.Size;
 public class PostUpdateRequest {
 
     @NotBlank(message = "제목은 필수입니다.")
-    @Size(min = 1, max = 15, message = "제목은 1글자 이상 15글자 이햐여야 합니다.")
+    @Size(min = 1, max = 15, message = "제목은 1글자 이상 15글자 이하여야 합니다.")
     private String title;
 
     @NotBlank(message = "내용은 필수입니다.")
-    @Size(min = 1, max = 1000, message = "제목은 1글자 이상 1000글자 이하여야 합니다.")
+    @Size(min = 1, max = 1000, message = "내용은 1글자 이상 1000글자 이하여야 합니다.")
     private String content;
 
     protected PostUpdateRequest() {
