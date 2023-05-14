425da695d2ea26adb722563619e0b78d4ad53d60
Ribo <ribo.ha1111@gmail.com>
2023-05-14 17:05:50 +0900

    docs: api 및 상태 코드 명세 추가

--- src/main/java/com/bulletinboard/post/controller/PostController.java
+++ src/main/java/com/bulletinboard/post/controller/PostController.java
@@ -5,6 +5,10 @@ import com.bulletinboard.post.dto.PostResponse;
 import com.bulletinboard.post.dto.PostUpdateRequest;
 import com.bulletinboard.post.exception.InvalidPostException;
 import com.bulletinboard.post.service.PostService;
+import com.bulletinboard.utils.ApiUtils;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
@@ -25,16 +29,29 @@ public class PostController {
 
     private final PostService postService;
 
+    @Operation(summary = "게시글 생성", description = "게시글을 생성합니다.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "201", description = "CREATED"),
+            @ApiResponse(responseCode = "400", description = "BAD REQUEST"),
+            @ApiResponse(responseCode = "500", description = "INTERNAL SERVER ERROR")
+    })
     @PostMapping
-    public ApiResponse<PostResponse> savePost(@Valid @RequestBody PostNewRequest postNewRequest) {
+    public ApiUtils.ApiResponse<PostResponse> savePost(@Valid @RequestBody PostNewRequest postNewRequest) {
         Long savedPostId = postService.savePost(postNewRequest);
         PostResponse postResponse = postService.findPostById(savedPostId);
 
         return success(postResponse, HttpStatus.CREATED);
     }
 
+    @Operation(summary = "게시글 모두 조회", description = "게시글을 모두 조회합니다.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "OK"),
+            @ApiResponse(responseCode = "404", description = "NOT FOUND"),
+            @ApiResponse(responseCode = "400", description = "BAD REQUEST"),
+            @ApiResponse(responseCode = "500", description = "INTERNAL SERVER ERROR")
+    })
     @GetMapping
-    public ApiResponse<Slice<PostResponse>> findPosts(
+    public ApiUtils.ApiResponse<Slice<PostResponse>> findPosts(
             @PageableDefault(size = 100, sort = "createdDate", direction = DESC) Pageable pageable) {
 
         if (pageable.getPageSize() > 100) {
@@ -45,8 +62,15 @@ public class PostController {
         return success(postResponses, HttpStatus.OK);
     }
 
+    @Operation(summary = "게시글 키워드로 조회", description = "게시글을 키워드로 조회합니다.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "OK"),
+            @ApiResponse(responseCode = "404", description = "NOT FOUND"),
+            @ApiResponse(responseCode = "400", description = "BAD REQUEST"),
+            @ApiResponse(responseCode = "500", description = "INTERNAL SERVER ERROR")
+    })
     @GetMapping("/word")
-    public ApiResponse<Slice<PostResponse>> findPostsByKeyword(
+    public ApiUtils.ApiResponse<Slice<PostResponse>> findPostsByKeyword(
             @RequestParam String keyword,
             @PageableDefault(size = 100, sort = "createdDate", direction = DESC) Pageable pageable) {
 
@@ -60,23 +84,42 @@ public class PostController {
         return success(postResponses, HttpStatus.OK);
     }
 
+    @Operation(summary = "게시글 조회", description = "하나의 게시글을 조회합니다.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "OK"),
+            @ApiResponse(responseCode = "404", description = "NOT FOUND"),
+            @ApiResponse(responseCode = "500", description = "INTERNAL SERVER ERROR")
+    })
     @GetMapping("/{id}")
-    public ApiResponse<PostResponse> findPostById(@PathVariable Long id) {
+    public ApiUtils.ApiResponse<PostResponse> findPostById(@PathVariable Long id) {
         PostResponse postResponse = postService.findPostById(id);
 
         return success(postResponse, HttpStatus.OK);
     }
 
+    @Operation(summary = "게시글 수정", description = "하나의 게시글을 수정합니다.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "OK"),
+            @ApiResponse(responseCode = "404", description = "NOT FOUND"),
+            @ApiResponse(responseCode = "400", description = "BAD REQUEST"),
+            @ApiResponse(responseCode = "500", description = "INTERNAL SERVER ERROR")
+    })
     @PutMapping("/{id}")
-    public ApiResponse<PostResponse> updatePostById(@PathVariable Long id, @RequestBody PostUpdateRequest postUpdateRequest) {
+    public ApiUtils.ApiResponse<PostResponse> updatePostById(@PathVariable Long id, @RequestBody PostUpdateRequest postUpdateRequest) {
         postService.updatePost(id, postUpdateRequest);
         PostResponse postResponse = postService.findPostById(id);
 
         return success(postResponse, HttpStatus.OK);
     }
 
+    @Operation(summary = "게시글 삭제", description = "하나의 게시글을 삭제합니다.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "OK"),
+            @ApiResponse(responseCode = "404", description = "NOT FOUND"),
+            @ApiResponse(responseCode = "500", description = "INTERNAL SERVER ERROR")
+    })
     @DeleteMapping("/{id}")
-    public ApiResponse<Object> deletePost(@PathVariable Long id) {
+    public ApiUtils.ApiResponse<Object> deletePost(@PathVariable Long id) {
         postService.deletePostById(id);
 
         return success(null, HttpStatus.NO_CONTENT);
