347a55a8761227fcf01afc647ed007af1e7b01d1
Ribo <ribo.ha1111@gmail.com>
2023-05-24 15:49:44 +0900

    refactor: 테스트와 api 한 곳에서 작성

--- src/test/java/com/bulletinboard/ControllerTestSupport.java
+++ src/test/java/com/bulletinboard/ControllerTestSupport.java
@@ -5,15 +5,24 @@ import com.bulletinboard.member.service.MemberService;
 import com.bulletinboard.post.controller.PostController;
 import com.bulletinboard.post.service.PostService;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
 
 @WebMvcTest(controllers = {
         PostController.class,
         MemberController.class
 })
+@ExtendWith(RestDocumentationExtension.class)
 public abstract class ControllerTestSupport {
 
     @Autowired
@@ -27,4 +36,11 @@ public abstract class ControllerTestSupport {
 
     @MockBean
     protected MemberService memberService;
+
+    @BeforeEach
+    void setUp(WebApplicationContext webApplicationContext, RestDocumentationContextProvider provider) {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
+                .apply(documentationConfiguration(provider))
+                .build();
+    }
 }
