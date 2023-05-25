1e4d7a2b62b1179e06cd3f3d648ec22db71aa224
Ribo <ribo.ha1111@gmail.com>
2023-05-25 10:07:28 +0900

    feat: jwt 인증 추가

---
+++ src/main/java/com/auth/dto/TokenInfo.java
@@ -0,0 +1,22 @@
+package com.auth.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+public class TokenInfo {
+
+    private String grantType;
+    private String accessToken;
+    private String refreshToken;
+
+    private TokenInfo() {
+    }
+
+    @Builder
+    private TokenInfo(String grantType, String accessToken, String refreshToken) {
+        this.grantType = grantType;
+        this.accessToken = accessToken;
+        this.refreshToken = refreshToken;
+    }
+}
