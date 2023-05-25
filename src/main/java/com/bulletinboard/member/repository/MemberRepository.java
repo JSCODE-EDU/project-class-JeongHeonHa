a56a85e09814e5261c925fe81ced1b5af39ce906
Ribo <ribo.ha1111@gmail.com>
2023-05-25 10:07:05 +0900

    feat: 이메일로 회원 조회 기능 추가

--- src/main/java/com/bulletinboard/member/repository/MemberRepository.java
+++ src/main/java/com/bulletinboard/member/repository/MemberRepository.java
@@ -5,8 +5,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
-    @Query("select count(*) > 0 from Member m where m.email.email = :email")
+    @Query("select count(*) > 0 from Member m where m.email.value = :email")
     boolean existsByEmail(@Param("email") String email);
+
+    Optional<Member> findByEmailValue(String email);
 }
